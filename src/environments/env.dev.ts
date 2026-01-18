91791fc5a56b7de9964250fd3fc995bdae703aff
Nathanael Iung <nathanaeliung@hotmail.com>
2026-01-17 21:23:18 -0300

    - Creating initial environment files

---
+++ src/environments/env.dev.ts
@@ -0,0 +1,4 @@
+export const env = {
+  PRODUCTION: false,
+  API: `http://localhost:3000/api/`
+};
---
+++ src/environments/env.main.ts
@@ -0,0 +1,4 @@
+export const env = {
+  PRODUCTION: true,
+  API: `http://localhost:3000/api/`
+};
