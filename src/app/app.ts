1fbcd2a47c225fffa0ae6de09de7e4d79d61b0b9
Nathanael Iung <nathanaeliung@hotmail.com>
2026-01-19 14:31:16 -0300

    - Adding loading state to the main app component - Adding route animation check to the main app html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
+import { slideInAnimation } from "./shared/utils/route-animations";
+import { LoadingService } from "./shared/services/loading/loading-service";
+import { Loading } from "./shared/components/loading/loading";
+import { CommonModule } from "@angular/common";
+import { Toast } from "./shared/components/toast/toast";
+import { MessageService } from "primeng/api";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    Loading,
+    CommonModule,
+    Toast
+  ],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  animations: [slideInAnimation],
+  providers: [MessageService]
 })
 export class App {
+
+  contexts = inject(ChildrenOutletContexts)
+
+  loadingService = inject(LoadingService)
+  loading$ = this.loadingService.listenLoadingState()
+
   protected readonly title = signal('protocolo-360');
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
+
 }
