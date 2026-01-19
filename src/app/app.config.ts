71a232c8f8c70db7bd8185372168648e0b2e6fad
Nathanael Iung <nathanaeliung@hotmail.com>
2026-01-19 10:58:10 -0300

    - Adding animations for transitions between routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,14 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { providePrimeNG } from "primeng/config";
 
 import Aura from '@primeuix/themes/aura';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: {
           preset: Aura,
